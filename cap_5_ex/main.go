113b55c3aa52933fadf6badc6ee036c97ac71634
joaooliveira247 <joaooliveira247@gmail.com>
2023-09-10 22:12:18 -0300

    :book:

---
+++ cap_5_ex/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sum(slice []int) int {
+	total := 0
+	for _, v := range slice {
+		total += v
+	}
+	return total
+}
+
+func half(x int) (int, bool) {
+	if x % 2 == 0 {
+		return x / 2, true
+	}
+	return x / 2, false
+}
+
+func greatter(values ... int) int {
+	if len(values) < 1 {
+		return values[0]
+	}
+	greatter := values[0]
+	for _, v := range values[1:] {
+		if v > greatter {
+			greatter = v
+		}
+	}
+	return greatter
+}
+
+func makeOddGenerator() func() uint {
+	i := uint(1)
+	return func() (ret uint) {
+		ret = i
+		i += 2
+		return
+	}
+}
+
+func fib(n uint) uint {
+	switch n {
+	case 0:
+		return 0
+	case 1:
+		return 1
+	default:
+		return fib(n - 1) + fib(n - 2)
+	}
+}
+
+func main() {
+	// a := makeOddGenerator()
+	// for i := 0; i < 10 ; i++ {
+	// 	fmt.Println(a())
+	// }
+	fmt.Println(fib(31))
+}
